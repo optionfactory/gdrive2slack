0c64c46ea189682b863cc0c99381c42cc5992025
teddyboar <teddy.boar@optionfactory.net>
2015-02-10 17:20:28 +0100

    ref: moved handlers out of ServeHttp

--- src/gdrive2slack/http.go
+++ src/gdrive2slack/http.go
@@ -2,14 +2,13 @@ package gdrive2slack
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 	"github.com/optionfactory/gdrive2slack/google"
 	"github.com/optionfactory/gdrive2slack/google/userinfo"
 	"github.com/optionfactory/gdrive2slack/slack"
 	"net/http"
-
-	"fmt"
 )
 
 type Request struct {
@@ -42,67 +41,72 @@ func ServeHttp(client *http.Client, registerChannel chan *SubscriptionAndAccessT
 		renderer.HTML(200, "index", indexData)
 	})
 	m.Put("/", func(renderer render.Render, req *http.Request) {
-		decoder := json.NewDecoder(req.Body)
-		var r Request
-		err := decoder.Decode(&r)
-		if err != nil {
-			renderer.JSON(400, &ErrResponse{err.Error()})
-			return
-		}
-		if r.GoogleCode == "" {
-			renderer.JSON(400, &ErrResponse{"Invalid oauth code for google"})
-			return
-		}
-		if r.SlackCode == "" {
-			renderer.JSON(400, &ErrResponse{"Invalid oauth code for slack"})
-			return
-		}
-		if r.Channel == "" {
-			r.Channel = "#general"
-		}
-		googleRefreshToken, googleAccessToken, status, err := google.NewAccessToken(configuration.Google, client, r.GoogleCode)
-		if status != google.Ok {
-			renderer.JSON(500, &ErrResponse{err.Error()})
-			return
-		}
-		slackAccessToken, ostatus, err := slack.NewAccessToken(configuration.Slack, client, r.SlackCode)
-		if ostatus != slack.OauthOk {
-			renderer.JSON(500, &ErrResponse{err.Error()})
-			return
-		}
-		gUserInfo, status, err := userinfo.GetUserInfo(client, googleAccessToken)
-		if status != google.Ok {
-			renderer.JSON(500, &ErrResponse{err.Error()})
-			return
-		}
-		sUserInfo, sstatus, err := slack.GetUserInfo(client, slackAccessToken)
-		if sstatus != slack.Ok {
-			renderer.JSON(500, &ErrResponse{err.Error()})
-			return
-		}
-
-		cstatus, err := slack.PostMessage(client, slackAccessToken, &slack.Message{
-			Channel:  r.Channel,
-			Username: "Google Drive",
-			Text:     fmt.Sprintf("A <%s|GDrive2Slack> integration has been configured by <@%s|%s>. Activities on Google Drive documents will be notified here.", configuration.Google.RedirectUri, sUserInfo.UserId, sUserInfo.User),
-			IconUrl:  "http://gdrive2slack.optionfactory.net/gdrive2slack.png",
-		})
-
-		registerChannel <- &SubscriptionAndAccessToken{
-			Subscription: &Subscription{
-				r.Channel,
-				slackAccessToken,
-				googleRefreshToken,
-				gUserInfo,
-				sUserInfo,
-			},
-			GoogleAccessToken: googleAccessToken,
-		}
-
-		renderer.JSON(200, map[string]interface{}{
-			"user":         gUserInfo,
-			"channelFound": cstatus == slack.Ok,
-		})
+		handleSubscriptionRequest(client, registerChannel, configuration, renderer, req)
 	})
 	m.RunOnAddr(configuration.BindAddress)
+}
+
+func handleSubscriptionRequest(client *http.Client, registerChannel chan *SubscriptionAndAccessToken, configuration *Configuration, renderer render.Render, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	var r Request
+	err := decoder.Decode(&r)
+	if err != nil {
+		renderer.JSON(400, &ErrResponse{err.Error()})
+		return
+	}
+	if r.GoogleCode == "" {
+		renderer.JSON(400, &ErrResponse{"Invalid oauth code for google"})
+		return
+	}
+	if r.SlackCode == "" {
+		renderer.JSON(400, &ErrResponse{"Invalid oauth code for slack"})
+		return
+	}
+	if r.Channel == "" {
+		r.Channel = "#general"
+	}
+	googleRefreshToken, googleAccessToken, status, err := google.NewAccessToken(configuration.Google, client, r.GoogleCode)
+	if status != google.Ok {
+		renderer.JSON(500, &ErrResponse{err.Error()})
+		return
+	}
+	slackAccessToken, ostatus, err := slack.NewAccessToken(configuration.Slack, client, r.SlackCode)
+	if ostatus != slack.OauthOk {
+		renderer.JSON(500, &ErrResponse{err.Error()})
+		return
+	}
+	gUserInfo, status, err := userinfo.GetUserInfo(client, googleAccessToken)
+	if status != google.Ok {
+		renderer.JSON(500, &ErrResponse{err.Error()})
+		return
+	}
+	sUserInfo, sstatus, err := slack.GetUserInfo(client, slackAccessToken)
+	if sstatus != slack.Ok {
+		renderer.JSON(500, &ErrResponse{err.Error()})
+		return
+	}
+
+	cstatus, err := slack.PostMessage(client, slackAccessToken, &slack.Message{
+		Channel:  r.Channel,
+		Username: "Google Drive",
+		Text:     fmt.Sprintf("A <%s|GDrive2Slack> integration has been configured by <@%s|%s>. Activities on Google Drive documents will be notified here.", configuration.Google.RedirectUri, sUserInfo.UserId, sUserInfo.User),
+		IconUrl:  "http://gdrive2slack.optionfactory.net/gdrive2slack.png",
+	})
+
+	registerChannel <- &SubscriptionAndAccessToken{
+		Subscription: &Subscription{
+			r.Channel,
+			slackAccessToken,
+			googleRefreshToken,
+			gUserInfo,
+			sUserInfo,
+		},
+		GoogleAccessToken: googleAccessToken,
+	}
+
+	renderer.JSON(200, map[string]interface{}{
+		"user":         gUserInfo,
+		"channelFound": cstatus == slack.Ok,
+	})
+
 }
